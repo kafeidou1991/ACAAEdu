162e7141a87ca0369afc7f17e9ab7d12b70378ae
zhangjingwei <zhangjingwei@wanglibank.com>
2018-02-06 10:50:51 +0800

    no message

---
+++ ACAAEdu/ACAAEdu/Main/Login&Regist/views/PlaceView.m
@@ -0,0 +1,21 @@
+//
+//  PlaceView.m
+//  ACAAEdu
+//
+//  Created by 张竟巍 on 2018/2/6.
+//  Copyright © 2018年 ACAA. All rights reserved.
+//
+
+#import "PlaceView.h"
+
+@implementation PlaceView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
