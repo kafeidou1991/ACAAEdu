efd908b1db1a67fdea92fe1569a390f65ee2a678
zhangjingwei <zhangjingwei@wanglibank.com>
2018-03-08 18:21:57 +0800

    购物车

---
+++ ACAAEdu/ACAAEdu/Main/Login&Regist/views/AEGoodsBasketView.h
@@ -0,0 +1,20 @@
+//
+//  AEGoodsBasketView.h
+//  ACAAEdu
+//
+//  Created by 张竟巍 on 2018/3/8.
+//  Copyright © 2018年 ACAA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AEGoodsBasketView : UIView
+//显示隐藏view
+- (void)showGoodsView;
+- (void)hiddenGoodsView;
+
+//刷新数据
+//- (void)uo
+
+
+@end
---
+++ ACAAEdu/ACAAEdu/Main/Login&Regist/views/AEGoodsBasketView.m
@@ -0,0 +1,21 @@
+//
+//  AEGoodsBasketView.m
+//  ACAAEdu
+//
+//  Created by 张竟巍 on 2018/3/8.
+//  Copyright © 2018年 ACAA. All rights reserved.
+//
+
+#import "AEGoodsBasketView.h"
+
+@implementation AEGoodsBasketView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
