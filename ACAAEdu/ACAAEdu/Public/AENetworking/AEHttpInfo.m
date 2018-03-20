714fa51a50adb3a0b4bbf0d2e558fde0d6aacc09
zhangjingwei <zhangjingwei@wanglibank.com>
2018-03-20 15:00:52 +0800

    更换启动图片

--- ACAAEdu/ACAAEdu/Public/AENetworking/AEHttpInfo.m
+++ ACAAEdu/ACAAEdu/Public/AENetworking/AEHttpInfo.m
@@ -16,8 +16,8 @@
         self.platform = @"ios";
         self.timestamp = [NSString stringWithFormat:@"%lu",(unsigned long)[NSDate date].timeIntervalSince1970];
 //        self.networkDomain = @"http://www.iww123.com/";
-        self.networkDomain = @"http://id.acaa.cn/";
-//        self.networkDomain = @"http://www.bagua9.com/";
+//        self.networkDomain = @"http://id.acaa.cn/";
+        self.networkDomain = @"http://www.bagua9.com/";
         self.apiToken = @"";
     }
     return self;
