c602fa1d1dc39db0dd6f0211d649f3786a272f92
张竟巍 <38251725@qq.com>
2019-01-21 22:20:22 +0800

    优化

--- ACAAEdu/ACAAEdu/Main/Exam/Controllers/AEACAAExamVC.m
+++ ACAAEdu/ACAAEdu/Main/Exam/Controllers/AEACAAExamVC.m
@@ -105,14 +105,14 @@
     }
     return [UIView new];
 }
-//- (UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section {
-//    UIView * footView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, 1)];
-//    footView.backgroundColor = [UIColor whiteColor];
-//    return footView;
-//}
-//- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
-//    return 1.f;
-//}
+- (UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section {
+    UIView * footView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, 1)];
+    footView.backgroundColor = [UIColor whiteColor];
+    return footView;
+}
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
+    return 1.f;
+}
 - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
     AEAcaaCategoryItem * item = self.dataSources[section];
     return item.subject.count > 0 ? 30.f : 0.00000001;
